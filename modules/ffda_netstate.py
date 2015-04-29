02e9cdf2dec07c9247f5bdedf25a73b223ea170b
Martin Weinelt <mweinelt@gmail.com>
2015-04-30 01:59:52 +0200

    ffda_netstate: integrate helper functions

--- modules/ffda_netstate.py
+++ modules/ffda_netstate.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
-import willie
-
-import requests
 import json
 import shelve
 import time
 import traceback
+from datetime import datetime
 
-from ffda_lib import pretty_date, day_changed
+import requests
+
+import willie
 
 
 def setup(bot):
-    hs = shelve.open("ffda-highscore.shelve", writeback=True)
+    hs = shelve.open("ffda-highscore", writeback=True)
 
     # total highscore
     if 'nodes' not in hs:
@@ -143,3 +143,53 @@ def highscore(bot, trigger):
                      hs['clients'], pretty_date(hs['clients_dt']))
     bot.say(msg)
     print(msg)
+
+
+def pretty_date(timestamp=None):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.now()
+    compare = None
+    if isinstance(timestamp, int):
+        compare = datetime.fromtimestamp(timestamp)
+    elif isinstance(timestamp, float):
+        compare = datetime.fromtimestamp(int(timestamp))
+    elif isinstance(timestamp, datetime):
+        compare = timestamp
+    elif not timestamp:
+        compare = now
+
+    diff = now - compare
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "gerade eben"
+        if second_diff < 60:
+            return "vor {0} Sekunden".format(second_diff)
+        if second_diff < 120:
+            return "vor einer Minute"
+        if second_diff < 3600:
+            return "vor {0} Minuten".format(second_diff / 60)
+        if second_diff < 7200:
+            return "vor einer Stunde"
+        if second_diff < 86400:
+            return "vor {0:.2f} Stunden".format(second_diff / 3600)
+    if day_diff == 1:
+        return "gestern"
+    if day_diff < 7:
+        return "vor {0} Tagen".format(day_diff)
+
+    return "am {0}".format(compare.strftime('%d.%m.%Y um %H:%M Uhr'))
+
+
+def day_changed(since):
+    then = datetime.fromtimestamp(since).strftime('%x')
+    return then != time.strftime('%x')
